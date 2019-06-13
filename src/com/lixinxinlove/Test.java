0a143c6e2f71c2a9930efe69caea8fdbe391eb04
lixinxin <895330766@qq.com>
2019-06-13 14:10:21 +0800

    添加并归排序

--- src/com/lixinxinlove/Test.java
+++ src/com/lixinxinlove/Test.java
@@ -1,11 +1,64 @@
 package com.lixinxinlove;
 
+import com.lixinxinlove.sort.InsertionSort;
+import com.lixinxinlove.sort.MergeSort;
+import com.lixinxinlove.sort.SelectionSort;
+
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * 归并排序
  */
 public class Test {
 
+    private static final int n = 10000000;
+
+    private static int[] getArr(int n, int l, int r) {
+
+        int[] arr = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = l + random.nextInt(r - l);
+        }
+        return arr;
+    }
+
+
     public static void main(String[] args) {
+
+        int[] arr = getArr(n, 10, n + 10);
+
+        int[] arr1 = Arrays.copyOf(arr, n);
+
+        int[] arr2 = Arrays.copyOf(arr, n);
+
+
+        System.out.print("归并排序：");
+        long st = System.currentTimeMillis();
+        MergeSort.mergeSort(arr1, arr1.length);
+        long et = System.currentTimeMillis();
+        System.out.println(et - st);
+
+
+        System.out.print("归并排序2：");
+        long st2 = System.currentTimeMillis();
+        MergeSort.mergeSort(arr2, arr2.length);
+        long et2 = System.currentTimeMillis();
+        System.out.println(et2 - st2);
+
+//        System.out.print("选择排序：");
+//        long st = System.currentTimeMillis();
+//        SelectionSort.selectionSort(arr, arr.length);
+//        long et = System.currentTimeMillis();
+//        System.out.println(et - st);
+//
+//        System.out.print("插入排序：");
+//        long st1 = System.currentTimeMillis();
+//        InsertionSort.insertionSort2(arr1, arr1.length);
+//        long et1 = System.currentTimeMillis();
+//        System.out.println(et1 - st1);
+
 
     }
 
--- src/com/lixinxinlove/sort/MergeSort.java
+++ src/com/lixinxinlove/sort/MergeSort.java
@@ -1,16 +1,86 @@
 package com.lixinxinlove.sort;
 
+/**
+ * 归并排序   n*log(n)
+ */
 public class MergeSort {
 
 
-    private static void mergeSort(int[] arr, int n) {
+    public static void mergeSort(int[] arr, int n) {
+        __mergeSort(arr, 0, n - 1);
+    }
+
+    public static void mergeSort2(int[] arr, int n) {
+        __mergeSort2(arr, 0, n - 1);
+    }
+
+    //递归使用归并排序,对 arr[l...r]的范围进行排序
+    private static void __mergeSort(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int mid = (l + r) / 2;   //如果 l+r 很大  会有问题  int 约见
+
+        __mergeSort(arr, l, mid);
+        __mergeSort(arr, mid + 1, r);
+        __merge(arr, l, mid, r);
+    }
+
+    private static void __mergeSort2(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int mid = (l + r) / 2;   //如果 l+r 很大  会有问题  int 约见
+
+        __mergeSort2(arr, l, mid);
+        __mergeSort2(arr, mid + 1, r);
+        if (arr[mid] > arr[mid + 1]) {
+            __merge(arr, l, mid, r);
+        }
+
+    }
 
 
+    //将arr [l...mid] 和 arr[mid+1...r] 两部分进行归并
+    private static void __merge(int[] arr, int l, int mid, int r) {
+
+        int aux[] = new int[r - l + 1]; //申请空间
+
+        for (int i = l; i <= r; i++) {
+            aux[i - l] = arr[i];
+        }
+
+        int i = l;
+        int j = mid + 1;
+
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
+                arr[k] = aux[j - l];
+                j++;
+            } else if (j > r) {
+                arr[k] = aux[i - l];
+                i++;
+            } else if (aux[i - l] < aux[j - l]) {
+                arr[k] = aux[i - l];
+                i++;
+            } else {
+                arr[k] = aux[j - l];
+                j++;
+            }
+        }
 
     }
 
 
     public static void main(String[] args) {
+
+        int[] arr = {9, 8, 7, 3, 6, 5, 2, 4, 2, 1, 0};
+        mergeSort(arr, arr.length);
+        for (int i : arr) {
+            System.out.print(i);
+        }
 
     }
 
